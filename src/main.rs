be753c48dacebe5ec076d0fba6395f4b60346c8f
maiavictor <srvictormaia@gmail.com>
2018-10-12 18:33:19 -0300

    Remove outdated comments

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,6 @@
 pub mod term;
 use term::*;
 
-/*
-data Nat : Type
-| succ(x : Nat) : Nat
-| zero          : Nat
-
-data Bool : Type
-| true  : Bool
-| false : Bool
-
-data Fin : (n : Nat) -> Type
-| zer : (n : Nat) -> Fin (suc n)
-| suc : (n : Nat, i : Fin n) -> Fin (suc n)
-
-def Bool_ind(P : (Bool -> *), t : P(tru), f : P(fal), b : Bool)
-    match b : Bool to P(self)
-    | tru => t
-    | fal => f
-
-def ind(P : (Nat -> Type), s : (n : Nat, P n -> P(Nat.succ(n))), z : P(Nat.zero), n : Nat)
-    case Nat n to P(self)
-    | succ(x) => s(x, induce(x)) -- for any x:TYPE, ind(x) -> P(x)
-    | zero    => z
-    
-def is_zero(a : Nat)
-    case Nat a to Bool
-    | succ(pred) false
-    | zero       true
-
-def add(a : Nat, b : Nat)
-    fold Nat a to Nat
-    | succ(add_a) (b : Nat) -> Suc(add_a(b))
-    | zero        (b : Nat) -> Zer
-
-data List(T : Type)
-    | cons : ...
-    | nil  : ...
-*/
-
-// Simple datatype declaration
-data Nat
-| succ(n : Nat) -> Nat
-| zero          -> Nat
-
-// Simple function
-def double(n : Nat)
-    match Nat n : Nat
-    | succ(x : Nat) => succ(succ(id(x)))
-    | zero          => zero
-
-// Datatype declaration
-data Vec(T : Set, len : Nat)
-| cons(T : Set, len : Nat, x : T, xs : Vec(T,n)) -> Vec(T, succ n)
-| nil(T : Set)                                   -> Vec(T, zero)
-
-// Function
-def id(T : Set, len : Nat, vec : Vec T len)
-    match Vec(T, len) vec : (T : Set, len : Nat) => Vect T len
-    | cons(T : Set, len : Nat, x : T, xs : Vect(T,len)) => cons(T, len, x, id(T, len, xs))
-    | nil(T : Set)                                      => zero
-
-
 fn main() -> Result<(), std::string::String> {
     let (term, defs) = from_string_slice("
         /False
