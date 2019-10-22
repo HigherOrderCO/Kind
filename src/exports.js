3edf1317a2fabd56d272ed23c3aa721e3d930dae
Victor Maia <srvictormaia@gmail.com>
2019-10-22 08:53:51 -0300

    Revert "simplify fm-lang by parametrizing the file loader"
    This reverts commit 6a905ed1359c9382c8bf9ebbf95fd70afb03e289.
    
    For some reason, `fm -s Base` wasn't recursivelly saving all imports,
    only the saved file itself. I'm reverting this commit temporarily until
    it is fixed.

--- src/exports.js
+++ src/exports.js
@@ -4,7 +4,6 @@ var fm = module.exports = {
   net: require("./fm-net.js"),
   to_net: require("./fm-to-net.js"),
   to_js: require("./fm-to-js.js"),
-  forall: require("./forall"),
   json: require("./fm-json"),
 };
 
