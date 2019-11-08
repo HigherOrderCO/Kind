bb62bc801af40ac6492851fb16522bd4101ba136
Bernardo Amorim <contato@bamorim.com>
2019-11-08 20:15:21 -0300

    feat: async preprocessed wesm loading

--- src/fm-num.js
+++ src/fm-num.js
@@ -1,6 +1,6 @@
 'use strict';
 const fs = require('fs');
-const ops_bytes = fs.readFileSync('./ops.wasm');
+const ops = require('./ops');
 
 // Comparison
 const EQ     = 0
@@ -107,184 +107,172 @@ function from_s32(x) {
     return [x, 0]
 }
 
-
-(async () => {
-  // Explicitly compile and then instantiate the wasm module.
-  const ops_module = await WebAssembly.compile(ops_bytes);
-  const ops_ = await WebAssembly.instantiate(ops_module);
-  const ops = ops_.exports
-  const mem = new Uint32Array(ops.memory.buffer, 0, 4)
-
-  // apply : {fn : i32, x : [i32, i32], y : [i32, i32]} -> [i32, i32]
-  function apply(fn, x, y) {
-    ops.store(x[0],x[1],y[0],y[1]);
-    //console.log("store: " + mem);
-    var res = [];
-    switch (fn) {
-      case EQ:      ops.eq();        break;
-      case NE:      ops.ne();        break;
-      case LT_S:    ops.lt_s();      break;
-      case LT_U:    ops.lt_u();      break;
-      case GT_S:    ops.gt_s();      break;
-      case GT_U:    ops.gt_u();      break;
-      case LE_S:    ops.le_s();      break;
-      case LE_U:    ops.le_u();      break;
-      case GE_S:    ops.ge_s();      break;
-      case GE_U:    ops.ge_u();      break;
-      case CLZ:     ops.clz();       break;
-      case CTZ:     ops.ctz();       break;
-      case POPCNT:  ops.popcnt();    break;
-      case SHL:     ops.shl();       break;
-      case SHR:     ops.shr();       break;
-      case SHR_S:   ops.shr_s();     break;
-      case ROTL:    ops.rotl();      break;
-      case ROTR:    ops.rotr();      break;
-      case AND:     ops.and();       break;
-      case OR:      ops.or();        break;
-      case XOR:     ops.xor();       break;
-      case ADD:     ops.add();       break;
-      case SUB:     ops.sub();       break;
-      case MUL:     ops.mul();       break;
-      case DIV_S:   ops.div_s();     break;
-      case DIV_U:   ops.div_u();     break;
-      case REM_S:   ops.rem_s();     break;
-      case REM_U:   ops.rem_u();     break;
-      case FABS:    ops.fabs();      break;
-      case FNEG:    ops.fneg();      break;
-      case FCEIL:   ops.fceil();     break;
-      case FFLOOR:  ops.ffloor();    break;
-      case FNRST:   ops.fnearest();  break;
-      case FSQRT:   ops.fsqrt();     break;
-      case FADD:    ops.fadd();      break;
-      case FSUB:    ops.fsub();      break;
-      case FMUL:    ops.fmul();      break;
-      case FDIV:    ops.fdiv();      break;
-      case FMIN:    ops.fmin();      break;
-      case FMAX:    ops.fmax();      break;
-      case FCPYSGN: ops.fcopysign(); break;
-      case FEQ:     ops.feq();       break;
-      case FNE:     ops.fne();       break;
-      case FLT:     ops.flt();       break;
-      case FGT:     ops.fgt();       break;
-      case FLE:     ops.fle();       break;
-      case FGE:     ops.fge();       break;
-      case EXT32_S: ops.ext32_s();   break;
-      case FTOS:    ops.ftos();      break;
-      case FTOU:    ops.ftou();      break;
-      case STOF:    ops.stof();      break;
-      case UTOF:    ops.utof();      break;
-      default: throw "[ERROR]\nInvalid OP1 function.";
-    }
-    res[0] = ops.load0();
-    res[1] = ops.load1();
-    //console.log("result: " + mem);
-    ops.mem_clear();
-    return (res);
-    }
-
-  function check_tests() {
-    function is(x,y) {
-      return (x[0] >>> 0 == y[0] >>> 0) && (x[1] >>> 0 == y[1] >>> 0)
-    };
-
-   const tests =
-    [ is(apply(EQ,[0,0],[0,0]), [1,0])
-    , is(apply(EQ,[1,0],[1,0]), [1,0])
-    , is(apply(EQ,[1,0],[0,1]), [0,0])
-    , is(apply(NE,[0,0],[0,0]), [0,0])
-    , is(apply(NE,[1,0],[1,0]), [0,0])
-    , is(apply(NE,[1,0],[0,1]), [1,0])
-    , is(apply(LT_S, from_s32(0), from_s32(1)), [1,0])
-    , is(apply(LT_S, from_s32(-1), from_s32(1)), [1,0])
-    , is(apply(LT_S, from_s32(-2), from_s32(-2)), [0,0])
-    , is(apply(LT_S, from_s32(-2), from_s32(-1)), [1,0])
-    , is(apply(LT_S, from_s32(-1), from_s32(-2)), [0,0])
-    , is(apply(LT_S, from_s32(1), from_s32(0)), [0,0])
-    , is(apply(LT_U, [1,0], [2,0]), [1,0])
-    , is(apply(LT_U, [2,0], [0,0]), [0,0])
-    , is(apply(GT_S, from_s32(0), from_s32(1)), [0,0])
-    , is(apply(GT_S, from_s32(-1), from_s32(1)), [0,0])
-    , is(apply(GT_S, from_s32(-2), from_s32(-1)), [0,0])
-    , is(apply(GT_S, from_s32(-2), from_s32(-2)), [0,0])
-    , is(apply(GT_S, from_s32(-1), from_s32(-2)), [1,0])
-    , is(apply(GT_S, from_s32(1), from_s32(0)), [1,0])
-    , is(apply(GT_U, [2,0], [1,0]), [1,0])
-    , is(apply(GT_U, [0,0], [1,0]), [0,0])
-    , is(apply(LE_S, from_s32(0), from_s32(1)), [1,0])
-    , is(apply(LE_S, from_s32(-1), from_s32(1)), [1,0])
-    , is(apply(LE_S, from_s32(-2), from_s32(-1)), [1,0])
-    , is(apply(LE_S, from_s32(-2), from_s32(-2)), [1,0])
-    , is(apply(LE_S, from_s32(-1), from_s32(-2)), [0,0])
-    , is(apply(LE_S, from_s32(1), from_s32(0)), [0,0])
-    , is(apply(LE_S, from_s32(1), from_s32(0)), [0,0])
-    , is(apply(LE_U, [2,0], [1,0]), [0,0])
-    , is(apply(LE_U, [0,0], [1,0]), [1,0])
-    , is(apply(GE_S, from_s32(0), from_s32(1)), [0,0])
-    , is(apply(GE_S, from_s32(-1), from_s32(1)), [0,0])
-    , is(apply(GE_S, from_s32(-2), from_s32(-2)), [1,0])
-    , is(apply(GE_S, from_s32(-2), from_s32(-1)), [0,0])
-    , is(apply(GE_S, from_s32(-1), from_s32(-2)), [1,0])
-    , is(apply(GE_S, from_s32(1), from_s32(0)), [1,0])
-    , is(apply(GE_U, [2,0], [1,0]), [1,0])
-    , is(apply(GE_U, [0,0], [1,0]), [0,0])
-    , is(apply(CLZ, [0,0], [0,0]), [64,0])
-    , is(apply(CLZ, [0,0], [4,0]), [61,0])
-    , is(apply(CTZ, [0,0], [0,0]), [64,0])
-    , is(apply(CTZ, [0,0], [4,0]), [2,0])
-    , is(apply(POPCNT, [0,0], [5,0]), [2,0])
-    , is(apply(SHL, [4,0], [1,0]), [8,0])
-    , is(apply(SHR, [4,0], [1,0]), [2,0])
-    , is(apply(SHR_S, [4,0], [1,0]), [2,0])
-    , is(apply(SHR_S, from_s32(-256), [1,0]), [-128, -1])
-    , is(apply(ROTL, [256,0], [1,0]), [512, 0])
-    , is(apply(ROTR, [256,0], [1,0]), [128, 0])
-    , is(apply(ROTR, [256,0], [1,0]), [128, 0])
-    , is(apply(AND, [1,0], [1,0]), [1,0])
-    , is(apply(OR, [1,0], [1,0]), [1,0])
-    , is(apply(XOR, [1,0], [1,0]), [0,0])
-    , is(apply(ADD, [1,1], [0xFFFFFFFF,1]), [0,3])
-    , is(apply(SUB, [0,3], [0xFFFFFFFF,1]), [1,1])
-    , is(apply(DIV_U, [0,1], [65536,0]), [65536,0])
-    , is(apply(DIV_S, from_s32(-65536), from_s32(-256)), [256,0])
-    , is(apply(REM_U, [25,1], [65536,0]), [25,0])
-    , is(apply(REM_S, from_s32(-65537), from_s32(-256)), from_s32(-1))
-    , is(apply(FABS, [0,0], from_f64(-100)), from_f64(100))
-    , is(apply(FNEG, [0,0], from_f64(-1)), from_f64(1))
-    , is(apply(FNEG, [0,0], from_f64(1)), from_f64(-1))
-    , is(to_f64(from_f64(-1)), to_f64(from_f64(-1)))
-    , is(apply(FCEIL, [0,0], from_f64(100.1)), from_f64(101))
-    , is(apply(FFLOOR, [0,0], from_f64(100.9)), from_f64(100))
-    , is(apply(FNRST, [0,0], from_f64(100.6)), from_f64(101))
-    , is(apply(FSQRT, [0,0], from_f64(100)), from_f64(10))
-    , is(apply(FADD, from_f64(100.1), from_f64(100.5)), from_f64(200.6))
-    , is(apply(FSUB, from_f64(200.6), from_f64(100.1)), from_f64(100.5))
-    , is(apply(FMUL, from_f64(200.6), from_f64(2)), from_f64(401.2))
-    , is(apply(FDIV, from_f64(401.2), from_f64(2)), from_f64(200.6))
-    , is(apply(FMIN, from_f64(100.1), from_f64(100.5)), from_f64(100.1))
-    , is(apply(FMAX, from_f64(100.1), from_f64(100.5)), from_f64(100.5))
-    , is(apply(FCPYSGN, from_f64(-100.1), from_f64(100.5)), from_f64(100.1))
-    , is(apply(FEQ, from_f64(100.1), from_f64(100.1)), [1,0])
-    , is(apply(FNE, from_f64(100.1), from_f64(100.0)), [1,0])
-    , is(apply(FLT, from_f64(100.1), from_f64(100.0)), [0,0])
-    , is(apply(FGT, from_f64(100.1), from_f64(100.0)), [1,0])
-    , is(apply(FLE, from_f64(100.1), from_f64(100.1)), [1,0])
-    , is(apply(FGE, from_f64(100.1), from_f64(100.1)), [1,0])
-    , is(apply(EXT32_S, [0,0], [1,0] ), [1,0])
-    , is(apply(EXT32_S, [0,0], [-1,0] ), [-1,-1])
-    , is(apply(FTOS, [0,0], from_f64(-1)), [-1,-1])
-    , is(apply(FTOU, [0,0], from_f64(1)), [1,0])
-    , is(apply(STOF, [0,0], [-1,-1]), from_f64(-1))
-    , is(apply(UTOF, [0,0], [1,0]), from_f64(1))
-    ];
-
-   let bad = tests.indexOf(false)
-    if (tests.reduce((x,y) => x && y,true))
-      console.log('\x1b[32m%s\x1b[0m', "TESTING: All tests pass");
-    else
-      console.error('\x1b[31m%s\x1b[0m',"TESTING: Test #" + bad + " failed")
+// apply : {fn : i32, x : [i32, i32], y : [i32, i32]} -> [i32, i32]
+function apply(fn, x, y) {
+  ops.store(x[0],x[1],y[0],y[1]);
+  //console.log("store: " + mem);
+  var res = [];
+  switch (fn) {
+    case EQ:      ops.eq();        break;
+    case NE:      ops.ne();        break;
+    case LT_S:    ops.lt_s();      break;
+    case LT_U:    ops.lt_u();      break;
+    case GT_S:    ops.gt_s();      break;
+    case GT_U:    ops.gt_u();      break;
+    case LE_S:    ops.le_s();      break;
+    case LE_U:    ops.le_u();      break;
+    case GE_S:    ops.ge_s();      break;
+    case GE_U:    ops.ge_u();      break;
+    case CLZ:     ops.clz();       break;
+    case CTZ:     ops.ctz();       break;
+    case POPCNT:  ops.popcnt();    break;
+    case SHL:     ops.shl();       break;
+    case SHR:     ops.shr();       break;
+    case SHR_S:   ops.shr_s();     break;
+    case ROTL:    ops.rotl();      break;
+    case ROTR:    ops.rotr();      break;
+    case AND:     ops.and();       break;
+    case OR:      ops.or();        break;
+    case XOR:     ops.xor();       break;
+    case ADD:     ops.add();       break;
+    case SUB:     ops.sub();       break;
+    case MUL:     ops.mul();       break;
+    case DIV_S:   ops.div_s();     break;
+    case DIV_U:   ops.div_u();     break;
+    case REM_S:   ops.rem_s();     break;
+    case REM_U:   ops.rem_u();     break;
+    case FABS:    ops.fabs();      break;
+    case FNEG:    ops.fneg();      break;
+    case FCEIL:   ops.fceil();     break;
+    case FFLOOR:  ops.ffloor();    break;
+    case FNRST:   ops.fnearest();  break;
+    case FSQRT:   ops.fsqrt();     break;
+    case FADD:    ops.fadd();      break;
+    case FSUB:    ops.fsub();      break;
+    case FMUL:    ops.fmul();      break;
+    case FDIV:    ops.fdiv();      break;
+    case FMIN:    ops.fmin();      break;
+    case FMAX:    ops.fmax();      break;
+    case FCPYSGN: ops.fcopysign(); break;
+    case FEQ:     ops.feq();       break;
+    case FNE:     ops.fne();       break;
+    case FLT:     ops.flt();       break;
+    case FGT:     ops.fgt();       break;
+    case FLE:     ops.fle();       break;
+    case FGE:     ops.fge();       break;
+    case EXT32_S: ops.ext32_s();   break;
+    case FTOS:    ops.ftos();      break;
+    case FTOU:    ops.ftou();      break;
+    case STOF:    ops.stof();      break;
+    case UTOF:    ops.utof();      break;
+    default: throw "[ERROR]\nInvalid OP1 function.";
   }
-  check_tests();
+  res[0] = ops.load0();
+  res[1] = ops.load1();
+  //console.log("result: " + mem);
+  ops.mem_clear();
+  return (res);
+  }
 
+function check_tests() {
+  function is(x,y) {
+    return (x[0] >>> 0 == y[0] >>> 0) && (x[1] >>> 0 == y[1] >>> 0)
+  };
 
-})();
+  const tests =
+  [ is(apply(EQ,[0,0],[0,0]), [1,0])
+  , is(apply(EQ,[1,0],[1,0]), [1,0])
+  , is(apply(EQ,[1,0],[0,1]), [0,0])
+  , is(apply(NE,[0,0],[0,0]), [0,0])
+  , is(apply(NE,[1,0],[1,0]), [0,0])
+  , is(apply(NE,[1,0],[0,1]), [1,0])
+  , is(apply(LT_S, from_s32(0), from_s32(1)), [1,0])
+  , is(apply(LT_S, from_s32(-1), from_s32(1)), [1,0])
+  , is(apply(LT_S, from_s32(-2), from_s32(-2)), [0,0])
+  , is(apply(LT_S, from_s32(-2), from_s32(-1)), [1,0])
+  , is(apply(LT_S, from_s32(-1), from_s32(-2)), [0,0])
+  , is(apply(LT_S, from_s32(1), from_s32(0)), [0,0])
+  , is(apply(LT_U, [1,0], [2,0]), [1,0])
+  , is(apply(LT_U, [2,0], [0,0]), [0,0])
+  , is(apply(GT_S, from_s32(0), from_s32(1)), [0,0])
+  , is(apply(GT_S, from_s32(-1), from_s32(1)), [0,0])
+  , is(apply(GT_S, from_s32(-2), from_s32(-1)), [0,0])
+  , is(apply(GT_S, from_s32(-2), from_s32(-2)), [0,0])
+  , is(apply(GT_S, from_s32(-1), from_s32(-2)), [1,0])
+  , is(apply(GT_S, from_s32(1), from_s32(0)), [1,0])
+  , is(apply(GT_U, [2,0], [1,0]), [1,0])
+  , is(apply(GT_U, [0,0], [1,0]), [0,0])
+  , is(apply(LE_S, from_s32(0), from_s32(1)), [1,0])
+  , is(apply(LE_S, from_s32(-1), from_s32(1)), [1,0])
+  , is(apply(LE_S, from_s32(-2), from_s32(-1)), [1,0])
+  , is(apply(LE_S, from_s32(-2), from_s32(-2)), [1,0])
+  , is(apply(LE_S, from_s32(-1), from_s32(-2)), [0,0])
+  , is(apply(LE_S, from_s32(1), from_s32(0)), [0,0])
+  , is(apply(LE_S, from_s32(1), from_s32(0)), [0,0])
+  , is(apply(LE_U, [2,0], [1,0]), [0,0])
+  , is(apply(LE_U, [0,0], [1,0]), [1,0])
+  , is(apply(GE_S, from_s32(0), from_s32(1)), [0,0])
+  , is(apply(GE_S, from_s32(-1), from_s32(1)), [0,0])
+  , is(apply(GE_S, from_s32(-2), from_s32(-2)), [1,0])
+  , is(apply(GE_S, from_s32(-2), from_s32(-1)), [0,0])
+  , is(apply(GE_S, from_s32(-1), from_s32(-2)), [1,0])
+  , is(apply(GE_S, from_s32(1), from_s32(0)), [1,0])
+  , is(apply(GE_U, [2,0], [1,0]), [1,0])
+  , is(apply(GE_U, [0,0], [1,0]), [0,0])
+  , is(apply(CLZ, [0,0], [0,0]), [64,0])
+  , is(apply(CLZ, [0,0], [4,0]), [61,0])
+  , is(apply(CTZ, [0,0], [0,0]), [64,0])
+  , is(apply(CTZ, [0,0], [4,0]), [2,0])
+  , is(apply(POPCNT, [0,0], [5,0]), [2,0])
+  , is(apply(SHL, [4,0], [1,0]), [8,0])
+  , is(apply(SHR, [4,0], [1,0]), [2,0])
+  , is(apply(SHR_S, [4,0], [1,0]), [2,0])
+  , is(apply(SHR_S, from_s32(-256), [1,0]), [-128, -1])
+  , is(apply(ROTL, [256,0], [1,0]), [512, 0])
+  , is(apply(ROTR, [256,0], [1,0]), [128, 0])
+  , is(apply(ROTR, [256,0], [1,0]), [128, 0])
+  , is(apply(AND, [1,0], [1,0]), [1,0])
+  , is(apply(OR, [1,0], [1,0]), [1,0])
+  , is(apply(XOR, [1,0], [1,0]), [0,0])
+  , is(apply(ADD, [1,1], [0xFFFFFFFF,1]), [0,3])
+  , is(apply(SUB, [0,3], [0xFFFFFFFF,1]), [1,1])
+  , is(apply(DIV_U, [0,1], [65536,0]), [65536,0])
+  , is(apply(DIV_S, from_s32(-65536), from_s32(-256)), [256,0])
+  , is(apply(REM_U, [25,1], [65536,0]), [25,0])
+  , is(apply(REM_S, from_s32(-65537), from_s32(-256)), from_s32(-1))
+  , is(apply(FABS, [0,0], from_f64(-100)), from_f64(100))
+  , is(apply(FNEG, [0,0], from_f64(-1)), from_f64(1))
+  , is(apply(FNEG, [0,0], from_f64(1)), from_f64(-1))
+  , is(to_f64(from_f64(-1)), to_f64(from_f64(-1)))
+  , is(apply(FCEIL, [0,0], from_f64(100.1)), from_f64(101))
+  , is(apply(FFLOOR, [0,0], from_f64(100.9)), from_f64(100))
+  , is(apply(FNRST, [0,0], from_f64(100.6)), from_f64(101))
+  , is(apply(FSQRT, [0,0], from_f64(100)), from_f64(10))
+  , is(apply(FADD, from_f64(100.1), from_f64(100.5)), from_f64(200.6))
+  , is(apply(FSUB, from_f64(200.6), from_f64(100.1)), from_f64(100.5))
+  , is(apply(FMUL, from_f64(200.6), from_f64(2)), from_f64(401.2))
+  , is(apply(FDIV, from_f64(401.2), from_f64(2)), from_f64(200.6))
+  , is(apply(FMIN, from_f64(100.1), from_f64(100.5)), from_f64(100.1))
+  , is(apply(FMAX, from_f64(100.1), from_f64(100.5)), from_f64(100.5))
+  , is(apply(FCPYSGN, from_f64(-100.1), from_f64(100.5)), from_f64(100.1))
+  , is(apply(FEQ, from_f64(100.1), from_f64(100.1)), [1,0])
+  , is(apply(FNE, from_f64(100.1), from_f64(100.0)), [1,0])
+  , is(apply(FLT, from_f64(100.1), from_f64(100.0)), [0,0])
+  , is(apply(FGT, from_f64(100.1), from_f64(100.0)), [1,0])
+  , is(apply(FLE, from_f64(100.1), from_f64(100.1)), [1,0])
+  , is(apply(FGE, from_f64(100.1), from_f64(100.1)), [1,0])
+  , is(apply(EXT32_S, [0,0], [1,0] ), [1,0])
+  , is(apply(EXT32_S, [0,0], [-1,0] ), [-1,-1])
+  , is(apply(FTOS, [0,0], from_f64(-1)), [-1,-1])
+  , is(apply(FTOU, [0,0], from_f64(1)), [1,0])
+  , is(apply(STOF, [0,0], [-1,-1]), from_f64(-1))
+  , is(apply(UTOF, [0,0], [1,0]), from_f64(1))
+  ];
 
+  let bad = tests.indexOf(false)
+  if (tests.reduce((x,y) => x && y,true))
+    console.log('\x1b[32m%s\x1b[0m', "TESTING: All tests pass");
+  else
+    console.error('\x1b[31m%s\x1b[0m',"TESTING: Test #" + bad + " failed")
+}
+check_tests();
---
+++ tools/wasm2js.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+const template = (src) => `
+  const src = '${src}';
+  let buf = null;
+
+  const isNode = typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
+  if (isNode) {
+    buf = Buffer.from(src, 'base64');
+  } else {
+    const raw = window.atob(src);
+    const rawLength = raw.length;
+    buf = new Uint8Array(new ArrayBuffer(rawLength));
+    for(let i = 0; i < rawLength; i++) {
+      buf[i] = raw.charCodeAt(i);
+    }
+  }
+
+  const mod = new WebAssembly.Module(buf);
+  const instance = new WebAssembly.Instance(mod);
+  module.exports = instance.exports;
+`.trim()
+
+const convert = async (wasm_path) => {
+  const match = wasm_path.match(/(.*)(\.wasm$)/);
+  if(!match){
+    console.log("File does not end with .wasm extension");
+    process.exit(1);
+  }
+
+  if(!fs.existsSync(wasm_path)) {
+    console.log("File does not exist");
+    process.exit(1);
+  }
+
+  const target_path = `${match[1]}.js`
+  const src = fs.readFileSync(wasm_path).toString('base64')
+  fs.writeFileSync(target_path, template(src));
+}
+
+if(!process.argv[2]) {
+  console.log("Please specify at least one .wasm file");
+  process.exit(1);
+}
+
+(async () => {
+  for(let i = 2; i < process.argv.length; i++) {
+    const path = process.argv[i];
+    console.log(`Converting ${path} to .js...`)
+    await convert(path);
+  }
+})()
