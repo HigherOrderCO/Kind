58aa7161c402ffd40d5abce71cc6ae83126b4bbb
Victor Maia <srvictormaia@gmail.com>
2019-12-16 20:57:04 -0300

    Fix cli.js file name

--- esm/fm-to-js.js
+++
@@ -1,4 +0,0 @@
-import './errors.js';
-import './stringify.js';
-import './core-e930ae7b.js';
-export { c as compile } from './fm-to-js-ed975676.js';
--- esm/version.js
+++
@@ -1,4 +0,0 @@
-// This should be replaced by rollup
-const version = "0.1.217";
-
-export default version;
