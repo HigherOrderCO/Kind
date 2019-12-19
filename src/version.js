b4f969032c3ce4e1c628896aaa808f708c194138
Bernardo Amorim <contato@bamorim.com>
2019-12-19 16:20:24 -0300

    move version to typescript

--- src/version.js
+++
@@ -1,3 +0,0 @@
-// This should be replaced by rollup
-const version = "__VERSION__";
-export default version;
---
+++ src/version.ts
@@ -0,0 +1,3 @@
+// This should be replaced by rollup
+const version: string = "__VERSION__";
+export default version;
