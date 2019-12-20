1dd3f5eb4d3c5827346e7ad2fb8aba34839961aa
Bernardo Amorim <contato@bamorim.com>
2019-12-20 20:33:30 -0300

    simplify build now that everything is TS

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,10 @@ var version = require('./package.json').version
 
 var tsProject = ts.createProject('tsconfig.json')
 
-gulp.task('build', gulp.series(
-  function build_ts() {
-    return gulp
-      .src(['src/**/*.js', 'src/**/*.ts'])
-      .pipe(replace('__FORMALITY_VERSION__', version))
-      .pipe(tsProject())
-      .pipe(gulp.dest('dist/'))
-  }, function copy_d_ts() {
-    return gulp.src('src/**/*.d.ts').pipe(gulp.dest('dist/'));
-  })
-);
+gulp.task('build', function () {
+  return gulp
+    .src('src/**/*.ts')
+    .pipe(replace('__FORMALITY_VERSION__', version))
+    .pipe(tsProject())
+    .pipe(gulp.dest('dist/'))
+});
