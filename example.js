c1b7d764ed764ffabae194adb3c1d7a0cadb4316
Victor Maia <srvictormaia@gmail.com>
2020-03-28 18:46:43 -0300

    Words on example.js

--- example.js
+++ example.js
@@ -1,55 +1,87 @@
 var fmc = require("./FormalityCore.js");
-
-// Base code
 var code = `
-  Bool : Type
-    self(P : (x : Bool) -> Type;) ->
+Bool : Type
+  self(P : (x : Bool) -> Type;) ->
+  (t : P(true)) ->
+  (f : P(false)) ->
+  P(self)
+
+true : Bool
+  (P;) => (t) => (f) => t
+
+false : Bool
+  (P;) => (t) => (f) => f
+  
+Nat : Type
+  self(P : (x : Nat) -> Type;) ->
+  (z : P(zero)) ->
+  (s : (pred : Nat) -> P(succ(pred))) ->
+  P(self)
+
+zero : Nat
+  (P;) => (z) => (s) => z
+
+succ : (n : Nat) -> Nat
+  (n) => (P;) => (z) => (s) => s(n)
+
+Bits : Type
+  self(P : (x : Bits) -> Type;) ->
+  (be : P(be)) ->
+  (b0 : (pred : Bits) -> P(b0(pred))) ->
+  (b1 : (pred : Bits) -> P(b1(pred))) ->
+  P(self)
+
+be : Bits
+  (P;) => (be) => (b0) => (b1) => be
+
+b0 : (bs : Bits) -> Bits
+  (bs) => (P;) => (be) => (b0) => (b1) => b0(bs)
+
+b1 : (bs : Bits) -> Bits
+  (bs) => (P;) => (be) => (b0) => (b1) => b1(bs)
+
+Word : (size : Nat) -> Type
+  (size) =>
+  self(P : (size: Nat) -> (x : Word(size)) -> Type;) ->
+  (we : P(zero)(we)) ->
+  (w0 : (size : Nat) -> (pred : Word(size)) -> P(succ(size))(w0(size)(pred))) ->
+  (w1 : (size : Nat) -> (pred : Word(size)) -> P(succ(size))(w1(size)(pred))) ->
+  P(size)(self)
+
+we : Word(zero)
+  (P;) => (we) => (w0) => (w1) =>
+  we
+
+w0 : (size : Nat) -> (ws : Word(size)) -> Word(succ(size))
+  (size) => (ws) => (P;) => (we) => (w0) => (w1) =>
+  w0(size)(ws)
+
+w1 : (size : Nat) -> (ws : Word(size)) -> Word(succ(size))
+  (size) => (ws) => (P;) => (we) => (w0) => (w1) =>
+  w1(size)(ws)
+
+double : (n : Nat) -> Nat
+  (n) => n((x) => Nat;)(zero)((pred) => succ(succ(double(pred))))
+
+case_bool
+  : (b : Bool) ->
+    (P : (x : Bool) -> Type;) ->
     (t : P(true)) ->
     (f : P(false)) ->
-    P(self)
+    P(b)
+  (b) => (P;) => (t) => (f) => b(P;)(t)(f)
 
-  true : Bool
-    (P;) => (t) => (f) => t
-
-  false : Bool
-    (P;) => (t) => (f) => f
-
-  Nat : Type
-    self(P : (x : Nat) -> Type;) ->
+fold_nat
+  : (n : Nat) ->
+    (P : (n : Nat) -> Type) ->
     (z : P(zero)) ->
-    (s : (pred : Nat) -> P(succ(pred))) ->
-    P(self)
+    (s : (n : Nat) -> (i : P(n)) -> P(succ(n))) ->
+    P(n)
+  (n) => (P) => (z) => (s) =>
+    n(P;)(z)((pred) => s(pred)(fold_nat(pred)(P;)(z)(s)))
 
-  zero : Nat
-    (P;) => (z) => (s) => z
-
-  succ : (n : Nat) -> Nat
-    (n) => (P;) => (z) => (s) => s(n)
-    
-  double : (n : Nat) -> Nat
-    (n) => n((x) => Nat;)(zero)((pred) => succ(succ(double(pred))))
-
-  case_bool
-    : (b : Bool) ->
-      (P : (x : Bool) -> Type;) ->
-      (t : P(true)) ->
-      (f : P(false)) ->
-      P(b)
-    (b) => (P;) => (t) => (f) => b(P;)(t)(f)
-
-  fold_nat
-    : (n : Nat) ->
-      (P : (n : Nat) -> Type) ->
-      (z : P(zero)) ->
-      (s : (n : Nat) -> (i : P(n)) -> P(succ(n))) ->
-      P(n)
-    (n) => (P) => (z) => (s) =>
-      n(P;)(z)((pred) => s(pred)(fold_nat(pred)(P;)(z)(s)))
-
-  main : Nat
-    let n = succ(succ(zero))
-    let f = double
-    f(n)
+main : Word(succ(succ(zero)))
+  w0(succ(zero))(w1(zero)(we))
 `;
 
 // Parses module
