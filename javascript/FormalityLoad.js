4d016c27d08bf0711f35f21e820278daaf563e77
Victor Maia <srvictormaia@gmail.com>
2020-06-15 17:18:24 -0300

    Return defs

--- javascript/FormalityLoad.js
+++ javascript/FormalityLoad.js
@@ -97,6 +97,8 @@ module.exports = ({XMLHttpRequest, fs, localStorage}) => {
               for (var new_def in new_defs) {
                 await load_and_typesynth(new_def, defs, show, debug);
               };
+
+              return defs;
             } catch (e) {
               throw e;
             }
