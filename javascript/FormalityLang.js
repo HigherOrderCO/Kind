592b6e2a51f3d3b8a044621712f3ba3810239cca
Victor Maia <srvictormaia@gmail.com>
2020-07-09 17:49:41 -0300

    Fix error message error

--- javascript/FormalityLang.js
+++ javascript/FormalityLang.js
@@ -1737,7 +1737,9 @@ function stringify_err(err, code) {
   }
   var index = 0;
   if (!err.ctx) {
-    if (__dirname.indexOf("vic/dev") !== -1) {
+    if (typeof err === "function") {
+      return err();
+    } else if (__dirname.indexOf("vic/dev") !== -1) {
       return err;
     } else {
       return "Undecidable.";
