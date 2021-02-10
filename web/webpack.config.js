c842cabf816dd857c393df3f2d4c065eaeb5f4bc
maiavictor <srvictormaia@gmail.com>
2021-02-09 22:09:21 -0300

    Move docs directory up

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   output: {
     //filename: 'index.[contenthash].js',
     filename: 'index.js',
-    path: path.resolve(__dirname, 'docs')
+    path: path.resolve(__dirname, '../docs')
   },
   performance: {
     hints: false,
