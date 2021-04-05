968a7e9090f711363e57694a5eb2b459010f1408
MaiaVictor <srvictormaia@gmail.com>
2021-04-05 18:17:16 -0300

    Web.Kaelin game state

--- web/src/apps/index.js
+++ web/src/apps/index.js
@@ -2,5 +2,4 @@ module.exports = {
   'Web.Demo': import('./Web.Demo.js'),
   'Web.Jogo': import('./Web.Jogo.js'),
   'Web.Kaelin': import('./Web.Kaelin.js'),
-  'Web.Test': import('./Web.Test.js'),
 }
