2474ef46f28bcb3b3ef483470316548bda5177e5
MaiaVictor <srvictormaia@gmail.com>
2021-05-23 15:19:51 -0300

    Untrack compiled js

--- web/src/apps/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'App.Hello': import('./App.Hello.js'),
-  'App.Kind': import('./App.Kind.js'),
-  'App.MiniMMO': import('./App.MiniMMO.js'),
-  'App.Playground': import('./App.Playground.js'),
-}
