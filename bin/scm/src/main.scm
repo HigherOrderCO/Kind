2a2eb5aaa2e4115078fd6e99059220a65b712654
Rígille S. B. Menezes <rigillesbmenezes@protonmail.com>
2021-05-27 15:34:37 -0300

    implement --scm parameter in cli

--- bin/scm/src/main.scm
+++ bin/scm/src/main.scm
@@ -1,6 +1,43 @@
 #!/usr/bin/env -S scheme --script
-(load "./kind.so")
+(load "~/repositories/kind/bin/scm/src/kind.scm")
+(define suffix?
+  (lambda (str suff)
+    (let ((suff_length (string-length suff))
+          (str_length (string-length str)))
+      (if (<= suff_length str_length)
+        (string=?
+          (substring str (- str_length suff_length) str_length)
+          suff)
+        #f))))
+
+(define prefix?
+  (lambda (str pref)
+    (let ((pref_length (string-length pref))
+          (str_length (string-length str)))
+      (if (<= pref_length str_length)
+        (string=?
+          (substring str 0 pref_length)
+          pref)
+        #f))))
+
 (let ([args (cdr (command-line))])
   (unless (null? args)
-    (let ((filename (car args)))
-      (run_io (Kind.api.io.check_file filename)))))
+    (let ((fst_arg (car args))
+          (tail_args (cdr args)))
+      (if (null? tail_args)
+        (if (suffix? fst_arg ".kind")
+          (run_io (Kind.api.io.check_file fst_arg))
+          (run_io (Kind.api.io.check_term fst_arg)))
+        (let ((snd_arg (car tail_args)))
+          (case snd_arg
+            ("--scm"
+              (display (run_io (Kind.api.io.term_to_scheme fst_arg))))
+            ;("--run")
+            ;("--show")
+            ;("--norm")
+            ;("--js")
+            ;("--hs")
+            ;("--fmc")
+            (else
+              (display "unrecognized cli argument: ")
+              (display snd_arg))))))))
