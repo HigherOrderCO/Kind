a09db5bf865728a2deb2f8bb4dbe0938000ac212
MaiaVictor <srvictormaia@gmail.com>
2021-05-27 21:41:43 -0300

    Serve empty file when not found

--- web/files/main.js
+++ web/files/main.js
@@ -2,4 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.use(express.static(path.join(__dirname, "..", "..", "base")));
+app.get('*', (req, res) => { res.send(''); });
 app.listen(7172)
+
+
+
