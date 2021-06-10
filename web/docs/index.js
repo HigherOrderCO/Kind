08a6360413c43067b6ede969a5da22c7723b25b0
MaiaVictor <srvictormaia@gmail.com>
2021-06-10 13:38:09 -0300

    Init Kaelin GC

--- web/docs/index.js
+++ web/docs/index.js
@@ -3535,7 +3535,7 @@ module.exports = {
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 module.exports = {
-  'App.Drawing': __webpack_require__.e(/* import() */ 22).then(__webpack_require__.t.bind(__webpack_require__, 22, 23)),
+  'App.Kaelin': __webpack_require__.e(/* import() */ 635).then(__webpack_require__.t.bind(__webpack_require__, 635, 23)),
 }
 
 
