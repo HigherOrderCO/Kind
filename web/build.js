40d2f8c195a733c371e7ac6336efc6f3d3764df1
Maisa <maisa.milena@gmail.com>
2021-07-22 21:30:46 -0300

    Update build.js to build single app without removing the others

--- web/build.js
+++ web/build.js
@@ -10,16 +10,21 @@ process.chdir(code_dir);
 var all_js_apps   = fs.readdirSync("apps").filter(x => x.startsWith("App."));
 process.chdir(kind_dir);
 var all_kind_apps = fs.readdirSync("App").filter(x => x.slice(-5) === ".kind");
-// var app = ["TicTacToe.kind"];
 var app = "";
 var compiled_apps = [];
 
-// Only build 1 App
-if (process.argv[2]) {
+console.log("Compiling apps:")
+if (process.argv[2]) { // Only build 1 App
   app = all_kind_apps.filter(name => {
-    return name.toLowerCase().indexOf(process.argv[2].toLowerCase()) !== -1
-  });
-  compiled_apps = compile_app(app);
+    const match = process.argv[2].toLowerCase().slice(4) // remove "App."
+    return name.toLowerCase().endsWith(match) 
+      || name.toLowerCase().endsWith(match+".kind");
+  })[0];
+  if (app) {
+    compiled_apps = compile_app(app);
+  } else {
+    console.log("[error] App "+process.argv[2]+" not found.");
+  }
 } else { // Build all Apps
   console.log("Tip: to build only 1 app, use \x1b[2mnode build.js app_name\x1b[0m.")
   for (var file of all_kind_apps) {
@@ -27,30 +32,36 @@ if (process.argv[2]) {
   }
 }
 
+// Compile app from ".kind" to ".js"
+// Save it in "src/apps/"
 function compile_app(name) {
   process.chdir(kind_dir);
   var name = "App."+name.slice(0,-5);
   console.log("- " + name);
-  
   try {
-    var code = String(execSync("kind "+name+" --js --module | js-beautify-temp-fix", {maxBuffer: 1024 * 1024 * 1024}));
+    var code = String(execSync("kind "+name+" --js --module | js-beautify-temp-fix", { maxBuffer: 1024 * 1024 * 1024}));
   } catch (e) {
     console.log("Couldn't compile " + file + ". Error:");
     console.log(e.toString());
   }
-
-  // Write compiled App
+  // Write compiled App file
   process.chdir(code_dir);
   fs.writeFileSync("apps/"+name+".js", code);
   return name;
 }
 
-// Export Apps in index.js
+// Write "src/app/index.js" to export the Apps
 process.chdir(code_dir);
 var index = "module.exports = {\n";
-const build_all = app === "";
-for (var app of build_all ? all_js_apps : all_js_apps.concat(app)) {
-  index += "  '" + app + "': import('./"+app+"'),\n";
+const add_line = (app) => "  '" + app + "': import('./"+app+"'),\n";
+if (app !== "" && app !== undefined) { // Check if need to add App to the export list
+  const app_export_format = "App."+app.slice(0,-5)+".js";
+  if (all_js_apps.includes(app_export_format)) all_js_apps.concat(app_export_format);
+}
+// Order Apps alphabetically
+all_js_apps.sort((a, b) => a.localeCompare(b)) 
+for (var app of all_js_apps ) {
+  index += add_line(app);
 }
 index += "}\n";
 fs.writeFileSync("apps/index.js", index);
@@ -58,7 +69,7 @@ fs.writeFileSync("apps/index.js", index);
 console.log("Building index.js...");
 exec("npm run build", function (err, stdout, stdin) {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
     console.log("Done.");
   }
