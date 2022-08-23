aef090e7c0df4b66cce0d01237ccfca89a5e3d63
Felipe g <chiyokuoni@gmail.com>
2022-08-23 09:55:13 -0300

    Removed append on the bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ echo "Building Kind2 type checker"
 # Probably we should just use git clone in Wikind?
 cd ../Wikind
 $KIND2 check Kind/TypeChecker.kind2
-$KIND2 to-hvm Kind/TypeChecker.kind2 >> ../Kind2/src/checker.hvm
+$KIND2 to-hvm Kind/TypeChecker.kind2 > ../Kind2/src/checker.hvm
 
 cd $CURRENT
 cargo build --release
