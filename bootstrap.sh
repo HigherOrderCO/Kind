701b0946d779c2e83faaad589fbc9d0cada408f9
Felipe g <chiyokuoni@gmail.com>
2022-08-23 09:30:46 -0300

    Improve bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,21 @@
+#!/bin/sh
+
+set -e
+
+CURRENT=$(realpath .)
+KIND2=$(realpath $CURRENT/target/release/kind2)
+CHECKER=$(realpath ../)
+
+echo "Building Kind2 without the new checker.hvm"
+
 cargo build --release
+
+echo "Building Kind2 type checker"
+
 # Probably we should just use git clone in Wikind?
 cd ../Wikind
-../Kind2/target/release/kind2 to-hvm Kind/TypeChecker.kind2 >> ../Kind2/src/checker.hvm
+$KIND2 check Kind/TypeChecker.kind2
+$KIND2 to-hvm Kind/TypeChecker.kind2 >> ../Kind2/src/checker.hvm
+
+cd $CURRENT
+cargo build --release
