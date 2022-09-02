5d504fc14de4956e70213861e70e23d796c2e472
Victor Maia <victor.taelin@gmail.com>
2022-09-02 19:15:29 -0300

    Rebuild

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,8 +13,10 @@ echo "Building Kind2 type checker"
 
 # Probably we should just use git clone in Wikind?
 cd ../Wikind
-$KIND2 check Kind/TypeChecker.kind2
+#$KIND2 check Kind/TypeChecker.kind2
 $KIND2 to-hvm Kind/TypeChecker.kind2 > ../Kind2/src/checker.hvm
 
-cd $CURRENT
-cargo build --release
+cargo install --path $CURRENT
+
+#cd $CURRENT
+#cargo build --release
