0cbfa9c1c4f57b92c844013fe2b2d2eee8a1cd89
felipegchi <chiyokuoni@gmail.com>
2022-12-16 13:31:59 -0300

    fix: workflow

--- tests/test_wikind.sh
+++ tests/test_wikind.sh
@@ -3,7 +3,7 @@ set -e
 git clone https://github.com/Kindelia/Wikind.git
 
 for file in ./Wikind/**/*.kind2; do
-    kind2 --root=Wikind check $file
+    cargo run --release -- --root=Wikind check $file
 done
 
 rm Wikind
