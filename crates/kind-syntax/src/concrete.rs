bdc5adda2dd06eb3a53904d7f69970145aa9c4ef
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-03-23 07:56:40 -0300

    refactor: move lexeme structures to to lexeme

--- crates/kind-syntax/src/concrete.rs
+++ crates/kind-syntax/src/concrete.rs
@@ -3,99 +3,16 @@
 //! It stores some tokens inside the tree in order to make it easier
 //! to reconstruct the entire program.
 
-use kind_span::Span;
 use num_bigint::BigUint;
 use thin_vec::ThinVec;
 
 use kind_lexer::tokens::Token;
 
 use crate::lexemes;
-
-pub enum Either<A, B> {
-    Left(A),
-    Right(B),
-}
-
-// Compounds
-#[derive(Debug)]
-pub struct Paren<T>(pub Token, pub T, pub Token);
-
-#[derive(Debug)]
-pub struct Bracket<T>(pub Token, pub T, pub Token);
-
-#[derive(Debug)]
-pub struct Brace<T>(pub Token, pub T, pub Token);
-
-#[derive(Debug)]
-pub struct AngleBracket<T>(pub Token, pub T, pub Token);
-
-#[derive(Debug)]
-pub struct Equal<T>(pub Token, pub T);
-
-#[derive(Debug)]
-pub struct Colon<T>(pub Token, pub T);
-
-#[derive(Debug)]
-pub struct Tokenized<T>(pub Token, pub T);
-
-impl<T> Paren<T> {
-    pub fn span(&self) -> Span {
-        self.0.span.mix(&self.2.span)
-    }
-}
-
-impl<T> Bracket<T> {
-    pub fn span(&self) -> Span {
-        self.0.span.mix(&self.2.span)
-    }
-}
-
-impl<T> Brace<T> {
-    pub fn span(&self) -> Span {
-        self.0.span.mix(&self.2.span)
-    }
-}
-
-impl<T> Tokenized<T> {
-    pub fn span(&self) -> Span {
-        self.0.span.clone()
-    }
-}
-
-impl<T> From<Tokenized<T>> for Item<T> {
-    fn from(val: Tokenized<T>) -> Self {
-        Item::new(val.span(), val.1)
-    }
-}
-
-// Concrete syntax tree
-
-#[derive(Debug)]
-pub struct Ident(pub Item<Tokenized<String>>);
-
-#[derive(Debug)]
-pub struct QualifiedIdent(pub Item<Tokenized<String>>);
-
-/// A localized data structure, it's useful to keep track of source code
-/// location.
-#[derive(Debug)]
-pub struct Item<T> {
-    pub data: T,
-    pub span: Span,
-}
-
-impl<T> Item<T> {
-    pub fn new(span: Span, data: T) -> Item<T> {
-        Item { data, span }
-    }
-
-    pub fn map<U>(self, fun: fn(T) -> U) -> Item<U> {
-        Item {
-            data: fun(self.data),
-            span: self.span,
-        }
-    }
-}
+use crate::lexemes::{
+    AngleBracket, Brace, Bracket, Colon, Either, Equal, Ident, Item, Paren, QualifiedIdent,
+    Tokenized,
+};
 
 #[derive(Debug)]
 pub enum AttributeStyleKind {
--- crates/kind-syntax/src/lexemes.rs
+++ crates/kind-syntax/src/lexemes.rs
@@ -1,4 +1,5 @@
 use kind_lexer::tokens::Token;
+use kind_span::Span;
 
 pub type Hash = Token;
 pub type Minus = Token;
@@ -22,3 +23,89 @@ pub type Match = Token;
 pub type Open = Token;
 pub type Do = Token;
 pub type Dot = Token;
+
+pub enum Either<A, B> {
+    Left(A),
+    Right(B),
+}
+
+// Compounds
+#[derive(Debug)]
+pub struct Paren<T>(pub Token, pub T, pub Token);
+
+#[derive(Debug)]
+pub struct Bracket<T>(pub Token, pub T, pub Token);
+
+#[derive(Debug)]
+pub struct Brace<T>(pub Token, pub T, pub Token);
+
+#[derive(Debug)]
+pub struct AngleBracket<T>(pub Token, pub T, pub Token);
+
+#[derive(Debug)]
+pub struct Equal<T>(pub Token, pub T);
+
+#[derive(Debug)]
+pub struct Colon<T>(pub Token, pub T);
+
+#[derive(Debug)]
+pub struct Tokenized<T>(pub Token, pub T);
+
+// Concrete syntax tree
+
+#[derive(Debug)]
+pub struct Ident(pub Item<Tokenized<String>>);
+
+#[derive(Debug)]
+pub struct QualifiedIdent(pub Item<Tokenized<String>>);
+
+/// A localized data structure, it's useful to keep track of source code
+/// location.
+#[derive(Debug)]
+pub struct Item<T> {
+    pub data: T,
+    pub span: Span,
+}
+
+impl<T> Paren<T> {
+    pub fn span(&self) -> Span {
+        self.0.span.mix(&self.2.span)
+    }
+}
+
+impl<T> Bracket<T> {
+    pub fn span(&self) -> Span {
+        self.0.span.mix(&self.2.span)
+    }
+}
+
+impl<T> Brace<T> {
+    pub fn span(&self) -> Span {
+        self.0.span.mix(&self.2.span)
+    }
+}
+
+impl<T> Tokenized<T> {
+    pub fn span(&self) -> Span {
+        self.0.span.clone()
+    }
+}
+
+impl<T> From<Tokenized<T>> for Item<T> {
+    fn from(val: Tokenized<T>) -> Self {
+        Item::new(val.span(), val.1)
+    }
+}
+
+impl<T> Item<T> {
+    pub fn new(span: Span, data: T) -> Item<T> {
+        Item { data, span }
+    }
+
+    pub fn map<U>(self, fun: fn(T) -> U) -> Item<U> {
+        Item {
+            data: fun(self.data),
+            span: self.span,
+        }
+    }
+}
