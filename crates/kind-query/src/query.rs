8512501203b72242651281a6bbfb3f6816dcbab2
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-03-28 21:27:22 -0300

    feat: add the prototype for queries

--- crates/kind-query/src/query.rs
+++ crates/kind-query/src/query.rs
@@ -1,13 +1,26 @@
+use std::path::PathBuf;
+
 use crate::fetch::Is;
 use kind_syntax::concrete;
 use kind_syntax::core;
+use thin_vec::ThinVec;
 
 /// A query is a request for a rule.
 pub enum Query<A> {
-    Module(Is<A, concrete::Module>, String),         // CST
+    SourceDirectories(Is<A, ThinVec<PathBuf>>, String), // File System
+    Source(Is<A, String>, PathBuf),                     // Text
+    Dependencies(Is<A, ThinVec<PathBuf>>, PathBuf),     // Dependencies
+    TransitiveDependencies(Is<A, ThinVec<PathBuf>>, PathBuf), // Dependencies
+
+    Module(Is<A, concrete::Module>, PathBuf),        // CST
     TopLevel(Is<A, concrete::TopLevel>, String),     // CST
     AbstractModule(Is<A, core::Module>, String),     // AST
     AbstractTopLevel(Is<A, core::TopLevel>, String), // AST
+}
+
+pub enum Fail {
+    UnboundModule(String),
+    UnboundTopLevel(String),
 }
 
 impl<A: std::hash::Hash> std::hash::Hash for Query<A> {
@@ -17,12 +30,16 @@ impl<A: std::hash::Hash> std::hash::Hash for Query<A> {
 }
 
 impl<A: std::hash::Hash> Query<A> {
-    pub fn rules(&self) -> Result<A, String> {
+    pub fn rules(&self) -> Result<A, Fail> {
         match self {
             Query::Module(_, _) => todo!(),
             Query::TopLevel(_, _) => todo!(),
             Query::AbstractModule(_, _) => todo!(),
             Query::AbstractTopLevel(_, _) => todo!(),
+            Query::SourceDirectories(_, _) => todo!(),
+            Query::Source(_, _) => todo!(),
+            Query::Dependencies(_, _) => todo!(),
+            Query::TransitiveDependencies(_, _) => todo!(),
         }
     }
 }
