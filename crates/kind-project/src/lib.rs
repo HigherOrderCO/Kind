2479b5d11e0ed9fb6f5e20034347fe773054ed68
Nicolas Abril <nicolas.abril@protonmail.ch>
2023-05-01 20:06:27 +0200

    feat: Add initial implementation of kind-project, with init command only

---
+++ crates/kind-project/src/lib.rs
@@ -0,0 +1,93 @@
+pub mod error;
+
+use anyhow::Context;
+use serde::{Deserialize, Serialize};
+use std::path::{Path, PathBuf};
+
+pub const PROJECT_CONFIG_FILE_NAME: &'static str = "kind.toml";
+pub const MAIN_FILE_NAME: &'static str = "Main.kind2";
+pub const MAIN_TEMPLATE: &'static str = "
+Main : Unit
+Main = Unit.new
+";
+
+#[derive(Deserialize, Serialize)]
+pub struct ProjectConfig {
+    pub name: String,
+    pub remote_url: String,
+    pub remote_version: String,
+}
+
+/// Searches for a project config file in the current directory and all parents.
+/// Returns Ok(Some(path)) if the file was found, with the path to it.
+/// Returns Ok(None) if the file was not found.
+/// Returns Err(err) if an error occurred while reading the filesystem.
+pub fn search_project_config_file() -> anyhow::Result<Option<PathBuf>> {
+    let mut path = PathBuf::from(Path::new(".")).canonicalize()?;
+    loop {
+        println!("debug: searching in {:?}", path);
+        // TODO: We shouldn't expect to have read access to all parent dirs
+        let files = path.read_dir().with_context(|| {
+            format!(
+                "Failed to read dir {:?} while searching for project config",
+                path.display()
+            )
+        })?;
+        for file in files {
+            let file = file.with_context(|| {
+                format!(
+                    "Failed to read file in dir {:?} while searching for project config",
+                    path.display()
+                )
+            })?;
+            if file.file_name() == PROJECT_CONFIG_FILE_NAME {
+                return Ok(Some(file.path()));
+            }
+        }
+
+        if let Some(parent) = path.parent() {
+            path = parent.into();
+        } else {
+            return Ok(None);
+        }
+    }
+}
+
+pub fn init_project(root: PathBuf, name: String) -> anyhow::Result<()> {
+    let config_path = root.join(PROJECT_CONFIG_FILE_NAME);
+    // Ok to create a project in the current directory
+    create_project_config_file(config_path, name.clone())?;
+    create_src_dir(root, name)?;
+    Ok(())
+}
+
+fn create_project_config_file(config_path: PathBuf, name: String) -> anyhow::Result<()> {
+    let config = ProjectConfig {
+        name,
+        // TODO: these should have an option to parametrized
+        // TODO: Need to decide on a good way of fetching files and searching versions
+        remote_url: "https://github.com/HigherOrderCO/Wikind.git".into(),
+        // TODO: By default should fetch the latest version
+        // TODO: Decide on a versioning scheme
+        remote_version: "".into(),
+    };
+    let toml_config = toml::to_string(&config).unwrap();
+    std::fs::write(&config_path, toml_config).with_context(|| {
+        format!(
+            "Failed to write project config file {}",
+            config_path.display()
+        )
+    })
+}
+
+fn create_src_dir(root: PathBuf, name: String) -> anyhow::Result<()> {
+    let src_dir = root.join(Path::new(&name));
+    // Create the src directory with the name of the project
+    std::fs::create_dir(&src_dir)
+        .with_context(|| format!("Failed to create src directory {}", src_dir.display()))?;
+    // Create a template Main file inside it
+    let main_path = src_dir.join(Path::new(MAIN_FILE_NAME));
+    std::fs::write(main_path, MAIN_TEMPLATE)
+        .with_context(|| format!("Failed to write Main file"))?;
+    Ok(())
+}
---
+++ crates/kind-project/src/main.rs
@@ -0,0 +1,174 @@
+use clap::{Parser, Subcommand};
+use kind_project::error::ProjectError;
+use kind_project::{init_project, ProjectConfig, PROJECT_CONFIG_FILE_NAME};
+use std::path::{Path, PathBuf};
+
+#[derive(Parser, Debug)]
+struct CliArgs {
+    /// Turn on the debugging information generated
+    /// by the compiler.
+    #[arg(short, long)]
+    pub debug: bool,
+
+    /// Show warning messages
+    #[arg(short, long)]
+    pub warning: bool,
+
+    /// Disable colors in error messages
+    #[arg(short, long)]
+    pub no_color: bool,
+
+    /// How much concurrency in HVM
+    #[arg(long)]
+    pub tids: Option<usize>,
+
+    /// Prints all of the functions and their evaluation
+    #[arg(short, long)]
+    pub trace: bool,
+
+    /// Only ascii characters in error messages
+    #[arg(short, long)]
+    pub ascii: bool,
+
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+// TODO: Think of a better way to interface the 2 applciations
+impl CliArgs {
+    fn as_kind_cli(self, root: PathBuf) -> anyhow::Result<kind2::Cli> {
+        let CliArgs {
+            debug,
+            warning,
+            no_color,
+            tids,
+            trace,
+            ascii,
+            command,
+        } = self;
+        let kind_cli = kind2::Cli {
+            config: None,
+            debug,
+            warning,
+            no_color,
+            tids,
+            trace,
+            ascii,
+            entrypoint: Some("Main".into()),
+            command: command.as_kind_command(&root)?,
+            root: Some(root),
+        };
+        Ok(kind_cli)
+    }
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    #[clap(aliases = &["i"])]
+    Init { name: String },
+
+    /// Checks the application
+    #[clap(aliases = &["c"])]
+    Check {
+        #[arg(short, long)]
+        coverage: bool,
+    },
+
+    /// Evaluates the application on Kind2
+    #[clap(aliases = &["er"])]
+    Eval,
+
+    #[clap(aliases = &["k"])]
+    ToKindCore,
+
+    #[clap(aliases = &["e"])]
+    Erase,
+
+    /// Runs the application on the HVM
+    #[clap(aliases = &["r"])]
+    Run,
+
+    /// Generates a checker (.hvm) for a file
+    #[clap(aliases = &["gc"])]
+    GenChecker {
+        #[arg(short, long)]
+        coverage: bool,
+    },
+
+    /// Stringifies Main and all its dependencies
+    #[clap(aliases = &["s"])]
+    Show,
+
+    /// Compiles the application to Kindelia (.kdl)
+    #[clap(aliases = &["kdl"])]
+    ToKDL {
+        /// If given, a namespace that goes before each compiled name. Can be at most 10 charaters long.
+        #[clap(long, aliases = &["ns"])]
+        namespace: Option<String>,
+    },
+
+    /// Compiles the application to HVM (.hvm)
+    #[clap(aliases = &["hvm"])]
+    ToHVM,
+}
+
+impl Command {
+    fn as_kind_command(self, root: &Path) -> anyhow::Result<kind2::Command> {
+        // TODO: handle non-utf8 paths properly
+        let file: String = root
+            .join(Path::new("Main.kind"))
+            .to_str()
+            .unwrap()
+            .to_string();
+        let kind_command = match self {
+            Command::Init { .. } => unreachable!(),
+            Command::Check { coverage } => kind2::Command::Check { coverage, file },
+            Command::Eval => kind2::Command::Eval { file },
+            Command::ToKindCore => kind2::Command::ToKindCore { file },
+            Command::Erase => kind2::Command::Erase { file },
+            Command::Run => kind2::Command::Run { file },
+            Command::GenChecker { coverage } => kind2::Command::GenChecker { coverage, file },
+            Command::Show => kind2::Command::Show { file },
+            Command::ToKDL { namespace } => kind2::Command::ToKDL { file, namespace },
+            Command::ToHVM => kind2::Command::ToHVM { file },
+        };
+        Ok(kind_command)
+    }
+}
+
+fn run_cli(args: CliArgs) -> anyhow::Result<()> {
+    let root = kind_project::search_project_config_file()?;
+    match &args.command {
+        Command::Init { name } => {
+            if let Some(root) = root {
+                Err(ProjectError::AlreadyInProject { root }.into())
+            } else {
+                init_project(PathBuf::from("."), name.clone())
+            }
+        }
+        _ => {
+            if let Some(root) = root {
+                let cfg_path = root.join(Path::new(PROJECT_CONFIG_FILE_NAME));
+                let cfg_data = String::from_utf8(std::fs::read(cfg_path)?)?;
+                let project_cfg: ProjectConfig = toml::from_str(&cfg_data)?;
+                let src_root = root.join(Path::new(&project_cfg.name));
+                let kind_cli = args.as_kind_cli(src_root)?;
+                kind2::run_cli(kind_cli)
+            } else {
+                todo!(); // Throw some error
+            }
+        }
+    }
+}
+
+pub fn main() {
+    let args = CliArgs::parse();
+    println!("{:?}", args);
+    match run_cli(args) {
+        Ok(()) => std::process::exit(0),
+        Err(e) => {
+            println!("Err: {}", e);
+            std::process::exit(1)
+        }
+    }
+}
