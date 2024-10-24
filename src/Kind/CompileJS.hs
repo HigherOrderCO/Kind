144f2dde0514da1bd2b97fc7baa235d571d2a88a
Victor Taelin <victor.taelin@gmail.com>
2024-10-24 18:40:27 -0300

    split compiler in two fns

--- src/Kind/CompileJS.hs
+++ src/Kind/CompileJS.hs
@@ -23,195 +23,185 @@ import Debug.Trace
 
 import Prelude hiding (EQ, LT, GT)
 
-useTypes :: Book -> Fill -> Term -> Maybe Term -> Int -> Term
-useTypes book fill term typx dep = go term where
-  go (All _ _ _) =
-    Set
+-- Compilable Term
+data CT
+  = CNul
+  | CLam String (CT -> CT)
+  | CApp CT CT
+  | CCon String [(String, CT)]
+  | CMat [(String, [String], CT)]
+  | CRef String
+  | CLet String CT (CT -> CT)
+  | CNum Word64
+  | CFlt Double
+  | COp2 Oper CT CT
+  | CSwi CT CT
+  | CLog CT CT
+  | CVar String Int
+  | CTxt String
+  | CLst [CT]
+  | CNat Integer
+
+termToCT :: Book -> Fill -> Term -> Maybe Term -> Int -> CT
+termToCT book fill term typx dep = go term where
   go (Lam nam bod) =
-    let bod' = \x -> useTypes book fill (bod x) Nothing (dep+1)
-    in Lam nam bod'
+    let bod' = \x -> termToCT book fill (bod (Var nam dep)) Nothing (dep+1)
+    in CLam nam bod'
   go (App fun arg) =
-    let fun' = useTypes book fill fun Nothing dep
-        arg' = useTypes book fill arg Nothing dep
-    in App fun' arg'
+    let fun' = termToCT book fill fun Nothing dep
+        arg' = termToCT book fill arg Nothing dep
+    in CApp fun' arg'
   go (Ann _ val typ) =
-    useTypes book fill val (Just typ) dep
+    termToCT book fill val (Just typ) dep
   go (Slf _ _ _) =
-    Set
+    CNul
   go (Ins val) =
-    useTypes book fill val typx dep
+    termToCT book fill val typx dep
   go (ADT _ _ _) =
-    Set
+    CNul
   go (Con nam arg) =
-    let adt = reduce book fill 2 (fromJust typx)
-        cts = getADTCts adt
-    in case lookup nam cts of
+    case lookup nam (getADTCts (reduce book fill 2 (fromJust typx))) of
       Just (Ctr _ tele) ->
-        let nms  = getTeleNames tele dep []
-            arg' = map (\ (nm, (_, tm)) -> (Just nm, useTypes book fill tm Nothing dep)) (zip nms arg)
-        in Con nam arg'
+        let fNames = getTeleNames tele dep []
+            fields = map (\ (f,t) -> (f, termToCT book fill t Nothing dep)) $ zip fNames (map snd arg)
+        in CCon nam fields
+      Nothing -> error $ "constructor-not-found:" ++ nam
   go (Mat cse) =
     case reduce book fill 2 (fromJust typx) of
-      All _ adt _ ->
-        let cts = getADTCts (reduce book fill 2 adt)
-        in Mat $ map (\(cnam, cbod) ->
-          if cnam == "_" then
-            (cnam, useTypes book fill (App cbod (Var "x" 0)) Nothing dep)
-          else case lookup cnam cts of
-            Just (Ctr _ tele) ->
-              let fieldNames = getTeleNames tele dep []
-                  cbodApps = foldl App cbod (map (\f -> Var ("x."++f) 0) fieldNames)
-              in (cnam, useTypes book fill cbodApps Nothing dep)
-            Nothing -> error $ "constructor-not-found:" ++ cnam
-        ) cse
-      _ -> error "Invalid type for match expression"
-  go (Ref nam) =
-    Ref nam
-  go (Let nam val bod) =
-    let val' = useTypes book fill val Nothing dep
-        bod' = \x -> useTypes book fill (bod x) Nothing (dep + 1)
-    in Let nam val' bod'
-  go (Use nam val bod) =
-    useTypes book fill (bod val) typx dep
-  go Set =
-    Set
-  go U64 =
-    U64
-  go F64 =
-    F64
-  go (Num val) =
-    Num val
-  go (Flt val) =
-    Flt val
-  go (Op2 opr fst snd) =
-    let fst' = useTypes book fill fst Nothing dep
-        snd' = useTypes book fill snd Nothing dep
-    in Op2 opr fst' snd'
-  go (Swi zer suc) =
-    let zer' = useTypes book fill zer Nothing dep
-        suc' = useTypes book fill suc Nothing dep
-    in Swi zer' suc'
-  go (Txt txt) =
-    Txt txt
-  go (Lst lst) =
-    let lst' = map (\x -> useTypes book fill x Nothing dep) lst
-    in Lst lst'
-  go (Nat val) =
-    Nat val
-  go (Hol _ _) =
-    Set
-  go (Met _ _) =
-    Set
-  go (Log msg nxt) =
-    let msg' = useTypes book fill msg Nothing dep
-        nxt' = useTypes book fill nxt Nothing dep
-    in Log msg' nxt'
-  go (Var nam _) =
-    Var nam 0
-  go (Src _ val) =
-    useTypes book fill val typx dep
-
-termToJS :: Book -> Fill -> Maybe String -> Term -> Int -> ST.State Int String
-termToJS book fill var term dep = go term where
+      (All _ adt _) ->
+        let adt' = reduce book fill 2 adt
+            cts  = getADTCts adt'
+            cses = map (\ (cnam, cbod) ->
+              if cnam == "_" then
+                (cnam, [], termToCT book fill cbod Nothing dep)
+              else case lookup cnam cts of
+                Just (Ctr _ tele) ->
+                  let fNames = getTeleNames tele dep []
+                  in (cnam, fNames, termToCT book fill cbod Nothing dep)
+                Nothing -> error $ "constructor-not-found:" ++ cnam) cse
+        in CMat cses
+      otherwise -> error "match-without-type"
   go (All _ _ _) =
-    ret var "null"
-  go (Lam nam bod) = do
-    let (names, bodyTerm) = lams term [] where
-          lams (Lam n b)   names = let uid = nameToJS n ++ "$" ++ show dep
-                                   in lams (b (Var uid dep)) (uid : names)
-          lams (Src _ v)   names = lams v names
-          lams (Ann _ v _) names = lams v names
-          lams tm          names = (reverse names, tm)
-    bodyName <- fresh
-    bodyStmt <- termToJS book fill (Just bodyName) bodyTerm (dep + length names)
-    ret var $ concat ["(", intercalate " => " names, " => {", bodyStmt, "return ", bodyName, ";", "})"]
-  go (App fun arg) = do
-    funExpr <- termToJS book fill Nothing fun dep
-    argExpr <- termToJS book fill Nothing arg dep
-    ret var $ concat ["(", funExpr, ")(", argExpr, ")"]
-  go (Ann _ val _) =
-    termToJS book fill var val dep
-  go (Slf _ _ _) =
-    ret var "null"
-  go (Ins val) =
-    termToJS book fill var val dep
-  go (ADT _ _ _) =
-    ret var "null"
-  go (Con nam arg) = do
-    argExprs <- forM arg $ \ (fname, argTerm) -> do
-      expr <- termToJS book fill Nothing argTerm dep
-      return (fromJust fname, expr)
-    let fields = concatMap (\ (fname, expr) -> ", " ++ fname ++ ": " ++ expr) argExprs
-    ret var $ concat ["({$: \"", nam, "\"", fields, "})"]
-  go (Mat cse) = do
-    cases <- forM cse $ \ (cnam, cbod) -> do
-      retName <- fresh
-      retStmt <- termToJS book fill (Just retName) cbod dep
-      return $ concat ["case \"", cnam, "\": { ", retStmt, " return " ++ retName ++ "; }"]
-    ret var $ concat ["(x => { switch (x.$) { ", unwords cases, " } })"]
+    CNul
   go (Ref nam) =
-    ret var $ nameToJS nam
-
+    CRef nam
   go (Let nam val bod) =
+    let val' = termToCT book fill val Nothing dep
+        bod' = \x -> termToCT book fill (bod (Var nam dep)) Nothing (dep+1)
+    in CLet nam val' bod'
+  go (Use nam val bod) =
+    termToCT book fill (bod val) typx dep
+  go Set =
+    CNul
+  go U64 =
+    CNul
+  go F64 =
+    CNul
+  go (Num val) =
+    CNum val
+  go (Flt val) =
+    CFlt val
+  go (Op2 opr fst snd) =
+    let fst' = termToCT book fill fst Nothing dep
+        snd' = termToCT book fill snd Nothing dep
+    in COp2 opr fst' snd'
+  go (Swi zer suc) =
+    let zer' = termToCT book fill zer Nothing dep
+        suc' = termToCT book fill suc Nothing dep
+    in CSwi zer' suc'
+  go (Txt txt) =
+    CTxt txt
+  go (Lst lst) =
+    CLst (map (\x -> termToCT book fill x Nothing dep) lst)
+  go (Nat val) =
+    CNat val
+  go (Hol _ _) =
+    CNul
+  go (Met _ _) =
+    CNul
+  go (Log msg nxt) =
+    let msg' = termToCT book fill msg Nothing dep
+        nxt' = termToCT book fill nxt Nothing dep
+    in CLog msg' nxt'
+  go (Var nam idx) =
+    CVar nam idx
+  go (Src _ val) =
+    termToCT book fill val typx dep
+
+ctToJS :: Book -> Fill -> Maybe String -> CT -> Int -> ST.State Int String
+ctToJS book fill var term dep = go term where
+  go CNul =
+    ret var "null"
+  go (CLam nam bod) = do
+    let uid = nameToJS nam ++ "$" ++ show dep
+    bodyName <- fresh
+    bodyStmt <- ctToJS book fill (Just bodyName) (bod (CVar uid dep)) (dep + 1)
+    ret var $ concat ["(", uid, " => {", bodyStmt, "return ", bodyName, ";})" ]
+  go (CApp fun arg) = do
+    funExpr <- ctToJS book fill Nothing fun dep
+    argExpr <- ctToJS book fill Nothing arg dep
+    ret var $ concat ["(", funExpr, ")(", argExpr, ")"]
+  go (CCon nam fields) = do
+    fieldExprs <- forM fields $ \ (fname, fterm) -> do
+      expr <- ctToJS book fill Nothing fterm dep
+      return (fname, expr)
+    let fields' = concatMap (\ (fname, expr) -> ", " ++ fname ++ ": " ++ expr) fieldExprs
+    ret var $ concat ["({$: \"", nam, "\"", fields', "})"]
+  go (CMat cses) = do
+    cases <- forM cses $ \ (cnam, fields, cbod) ->
+      if cnam == "_" then do
+        retName <- fresh
+        retStmt <- ctToJS book fill (Just retName) (CApp cbod (CVar "x" 0)) dep
+        return $ concat ["default: { " ++ retStmt, " return " ++ retName ++ "; }"]
+      else do
+        let bod = foldl CApp cbod (map (\f -> (CVar ("x."++f) 0)) fields)
+        retName <- fresh
+        retStmt <- ctToJS book fill (Just retName) bod dep
+        return $ concat ["case \"", cnam, "\": { ", retStmt, " return " ++ retName ++ "; }"]
+    ret var $ concat ["(x => { switch (x.$) { ", unwords cases, " } })"]
+  go (CRef nam) =
+    ret var $ nameToJS nam
+  go (CLet nam val bod) =
     case var of
       Just var -> do
         let uid = nameToJS nam ++ "$" ++ show dep
-        valExpr <- termToJS book fill (Just uid) val dep
-        bodExpr <- termToJS book fill (Just var) (bod (Var uid dep)) (dep + 1)
+        valExpr <- ctToJS book fill (Just uid) val dep
+        bodExpr <- ctToJS book fill (Just var) (bod (CVar uid dep)) (dep + 1)
         return $ concat [valExpr, bodExpr]
       Nothing -> do
         let uid = nameToJS nam ++ "$" ++ show dep
-        valExpr <- termToJS book fill (Just uid) val dep
-        bodExpr <- termToJS book fill Nothing (bod (Var uid dep)) (dep + 1)
+        valExpr <- ctToJS book fill (Just uid) val dep
+        bodExpr <- ctToJS book fill Nothing (bod (CVar uid dep)) (dep + 1)
         return $ concat ["(() => {", valExpr, "return ", bodExpr, ";})()"]
-  go (Use nam val bod) =
-    termToJS book fill var (bod val) dep
-  go Set =
-    ret var "null"
-  go U64 =
-    ret var "null"
-  go F64 =
-    ret var "null"
-  go (Num val) =
+  go (CNum val) =
     ret var $ show val
-  go (Flt val) =
+  go (CFlt val) =
     ret var $ show val
-  go (Op2 opr fst snd) = do
+  go (COp2 opr fst snd) = do
     let opr' = operToJS opr
-    fstExpr <- termToJS book fill Nothing fst dep
-    sndExpr <- termToJS book fill Nothing snd dep
+    fstExpr <- ctToJS book fill Nothing fst dep
+    sndExpr <- ctToJS book fill Nothing snd dep
     ret var $ concat ["((", fstExpr, " ", opr', " ", sndExpr, ") >>> 0)"]
-  go (Swi zer suc) = do
-    zerExpr <- termToJS book fill Nothing zer dep
-    sucExpr <- termToJS book fill Nothing suc dep
+  go (CSwi zer suc) = do
+    zerExpr <- ctToJS book fill Nothing zer dep
+    sucExpr <- ctToJS book fill Nothing suc dep
     ret var $ concat ["((x => x === 0 ? ", zerExpr, " : ", sucExpr, "(x - 1)))"]
-  go (Txt txt) =
-    ret var $ "JSTR_TO_LIST(`" ++ txt ++ "`)"
-  go (Lst lst) = do
-    let cons = \x acc -> Con "Cons" [(Just "head", x), (Just "tail", acc)]
-        nil  = Con "Nil" []
-    termToJS book fill var (foldr cons nil lst) dep
-  go (Nat val) = do
-    let succ = \x -> Con "Succ" [(Just "pred", x)]
-        zero = Con "Zero" []
-    termToJS book fill var (foldr (\_ acc -> succ acc) zero [1..val]) dep
-  go (Hol _ _) =
-    ret var "null"
-  go (Met _ _) =
-    ret var "null"
-  go (Log msg nxt) = do
-    msgExpr <- termToJS book fill Nothing msg dep
-    nxtExpr <- termToJS book fill Nothing nxt dep
+  go (CLog msg nxt) = do
+    msgExpr <- ctToJS book fill Nothing msg dep
+    nxtExpr <- ctToJS book fill Nothing nxt dep
     ret var $ concat ["(console.log(LIST_TO_JSTR(", msgExpr, ")), ", nxtExpr, ")"]
-  go (Var nam _) =
+  go (CVar nam _) =
     ret var nam
-  go (Src _ val) =
-    termToJS book fill var val dep
-
-
-
-
-
+  go (CTxt txt) =
+    ret var $ "JSTR_TO_LIST(`" ++ txt ++ "`)"
+  go (CLst lst) =
+    let cons = \x acc -> CCon "Cons" [("head", x), ("tail", acc)]
+        nil  = CCon "Nil" []
+    in  ctToJS book fill var (foldr cons nil lst) dep
+  go (CNat val) =
+    let succ = \x -> CCon "Succ" [("pred", x)]
+        zero = CCon "Zero" []
+    in  ctToJS book fill var (foldr (\_ acc -> succ acc) zero [1..val]) dep
 
 operToJS :: Oper -> String
 operToJS ADD = "+"
@@ -277,10 +267,55 @@ compileJS book = prelude ++ "\n\n" ++ concatMap compileDef (topoSortBook book) w
     case envRun (doAnnotate term) book of
       Done _ (term, fill) ->
         let uid = nameToJS name
-            val = bind term []
-            adj = useTypes book fill val Nothing 0
-            def = ST.evalState (termToJS book fill (Just uid) adj 0) 0
+            ct  = bindCT (termToCT book fill (bind term []) Nothing 0) []
+            def = ST.evalState (ctToJS book fill (Just uid) ct 0) 0
         in def ++ "\n\n"
       Fail _ ->
         error $ "COMPILATION_ERROR: " ++ name ++ " isn't well-typed."
 
+bindCT :: CT -> [(String,CT)] -> CT
+bindCT CNul ctx = CNul
+bindCT (CLam nam bod) ctx =
+  let bod' = \x -> bindCT (bod (CVar nam 0)) ((nam, x) : ctx) in
+  CLam nam bod'
+bindCT (CApp fun arg) ctx =
+  let fun' = bindCT fun ctx in
+  let arg' = bindCT arg ctx in
+  CApp fun' arg'
+bindCT (CCon nam fields) ctx =
+  let fields' = map (\(f, x) -> (f, bindCT x ctx)) fields in
+  CCon nam fields'
+bindCT (CMat cases) ctx =
+  let cases' = map (\(cn,fs,cb) -> (cn, fs, bindCT cb ctx)) cases in
+  CMat cases'
+bindCT (CRef nam) ctx =
+  case lookup nam ctx of
+    Just x  -> x
+    Nothing -> CRef nam
+bindCT (CLet nam val bod) ctx =
+  let val' = bindCT val ctx in
+  let bod' = \x -> bindCT (bod (CVar nam 0)) ((nam, x) : ctx) in
+  CLet nam val' bod'
+bindCT (CNum val) ctx = CNum val
+bindCT (CFlt val) ctx = CFlt val
+bindCT (COp2 opr fst snd) ctx =
+  let fst' = bindCT fst ctx in
+  let snd' = bindCT snd ctx in
+  COp2 opr fst' snd'
+bindCT (CSwi zer suc) ctx =
+  let zer' = bindCT zer ctx in
+  let suc' = bindCT suc ctx in
+  CSwi zer' suc'
+bindCT (CLog msg nxt) ctx =
+  let msg' = bindCT msg ctx in
+  let nxt' = bindCT nxt ctx in
+  CLog msg' nxt'
+bindCT (CVar nam idx) ctx =
+  case lookup nam ctx of
+    Just x  -> x
+    Nothing -> CVar nam idx
+bindCT (CTxt txt) ctx = CTxt txt
+bindCT (CLst lst) ctx =
+  let lst' = map (\x -> bindCT x ctx) lst in
+  CLst lst'
+bindCT (CNat val) ctx = CNat val
